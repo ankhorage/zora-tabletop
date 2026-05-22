dd1955b81466bf7fa6d95a12d578ef2106f58608
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-05-22 14:19:19 +0200

    feat: export tabletop primitives

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-/* todo */
+export type { CardBackProps } from './components/card-back';
+export { CardBack } from './components/card-back';
+export type { CardHandProps } from './components/card-hand';
+export { CardHand } from './components/card-hand';
+export type { PlayingCardProps } from './components/playing-card';
+export { PlayingCard } from './components/playing-card';
+export type { TabletopTableProps } from './components/tabletop-table';
+export { TabletopTable } from './components/tabletop-table';
+export type { TabletopColorOverrides, TabletopColorScheme } from './colors';
+export { createTabletopColorScheme } from './colors';
+export type {
+  PlayingCardSuit,
+  PlayingCardValue,
+  TabletopCardSize,
+  TabletopSeatCount,
+  TabletopSeatState,
+  TabletopShape,
+} from './types';
